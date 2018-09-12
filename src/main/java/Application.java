fdcbec7ffe36ace0a4c5dbaa5b990d9aeec9a2f6
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-09-12 21:21:05 +0800

    工程初始化

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by yangmingquan on 2018/9/12.
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/config/SwaggerUi.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yangmingquan on 2018/9/12.
+ */
+@Configuration
+public class SwaggerUi {
+}
